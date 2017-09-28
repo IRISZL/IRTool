5902ee6c7e740bd5754843543911bff09902539e
irisZL <IrisZL@192.168.0.2>
2017-09-28 22:48:38 +0800

    Initial Commit

---
+++ IRTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IRTools
+//
+//  Created by irisZL on 17/9/28.
+//  Copyright © 2017年 IRISZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IRTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IRTools
+//
+//  Created by irisZL on 17/9/28.
+//  Copyright © 2017年 IRISZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
