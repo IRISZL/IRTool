67da7de7562af989331d506083bd89107befe7a2
irisZL <821787401@qq.com>
2017-10-19 21:48:59 +0800

    增加测试代码

--- IRTools/ViewController.m
+++ IRTools/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "IRTools.h"
+#import "IRColorViewController.h"
+#import "IRUIViewController.h"
+#import "IRStringViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray<NSString *> *cellData;
+
 
 @end
 
@@ -17,7 +24,74 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"测试测试";
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [IRTools getScreenWidth], [IRTools getScreenHeight]) style:UITableViewStylePlain];
+    tableView.showsVerticalScrollIndicator = NO;
+    tableView.showsHorizontalScrollIndicator = NO;
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    [self.view addSubview:tableView];
+    
+    
+    _cellData = @[@"UIColor+Hex", @"UIView+Frame",@"NSString+Helper",@"NSAttributedString+Helpers", @"IRDateTools", @"IRDeviceTools", @"UIImage+helpers", @"UIView+Helpers", @"IRTools", @"IRFileTools"];
 }
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    static NSString *identify = @"UITableViewCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identify];
+    if (cell == nil) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identify];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    cell.textLabel.text = _cellData[indexPath.row];
+    
+    
+    
+    return cell;
+    
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _cellData.count;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView  deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSUInteger row = indexPath.row;
+    UIViewController *viewController = nil;
+    
+    switch (row) {
+        case 0: // UIColor + Helper
+            
+            viewController = [[IRColorViewController alloc] init];
+            break;
+        case 1: // UIView+Frame
+        case 7: // UIView+Helpers
+            viewController = [[IRUIViewController alloc] init];
+            break;
+        case 2: // NSString+Helper
+            viewController = [[IRStringViewController alloc] init];
+            break;
+        default:
+            break;
+    }
+    
+    
+    [self.navigationController pushViewController:viewController animated:YES];
+    
+    
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning {
